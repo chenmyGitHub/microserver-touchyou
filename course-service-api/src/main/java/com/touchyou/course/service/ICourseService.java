92aa06842b9d0887402a78a4bb103b9148605747
chenmy <568780909@qq.com>
2020-03-28 20:20:57 +0800

    init - course -service

---
+++ course-service-api/src/main/java/com/touchyou/course/service/ICourseService.java
@@ -0,0 +1,13 @@
+package com.touchyou.course.service;
+
+import com.touchyou.course.dto.CourseDTO;
+
+import java.util.List;
+
+/**
+ * Created by Michael on 2017/11/3.
+ */
+public interface ICourseService {
+
+    List<CourseDTO> courseList();
+}
